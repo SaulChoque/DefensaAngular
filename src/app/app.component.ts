2ba4e6afdc131f92d9b1bc3c60dde896135ee72d
Saul Mijael Choquehuanca Huanca <schoquehuancah@fcpn.edu.bo>
2024-06-07 18:20:07 -0400

    Mayor update

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { SliderComponent } from './slider/slider.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, BodyComponent, FooterComponent, MenuComponent, SliderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'choquehuanca';
+}
